e13f3a4aea5d98444babb8625144331d1ba7df41
Jason Wang <jasonwang@victoryfintech.com.hk>
2022-05-04 22:23:27 +0800

    Add login page

--- acc-web/src/pages/login/index.tsx
+++ acc-web/src/pages/login/index.tsx
@@ -1,7 +1,10 @@
-import {Row, Col} from "antd";
+import {Row, Col, Tabs, Form, Input, Button, Space, Divider} from "antd";
 
 import React from "react";
 import style from "@/pages/login/style.less";
+import {LockOutlined, MailOutlined, UserOutlined} from "@ant-design/icons";
+
+const {TabPane} = Tabs;
 
 export default () => {
   return (
@@ -9,26 +12,67 @@ export default () => {
       <div className={style.headerWrapper}>
         <Row className={style.header}>
           <Col flex="100px" style={{margin: 'auto'}}>
-            <img src={'./logo.png'} width={140} height={50}/>
+            <img src={'./logo3.png'} width={170} height={45}/>
           </Col>
           <Col flex="auto">
-
+            <a style={{float: "right", marginTop: 15}}>注册</a>
           </Col>
         </Row>
       </div>
       <div className={style.mainWrapper}>
         <div className={style.main}>
-            <Row>
-              <Col span={12}>
-                image
-              </Col>
-              <Col span={12}>
-                input
-              </Col>
-            </Row>
+          <Row>
+            <Col span={16}>
+              <img src="./login2.png" width='500px' height='350px'/>
+            </Col>
+            <Col span={8}>
+              <Tabs>
+                <TabPane tab="账户登录" key="account">
+                  <Form name="account">
+                    <Form.Item name="username" rules={[{required: true, message: '请输入您的用户名'}]}>
+                      <Input prefix={<UserOutlined/>} placeholder="请输入用户名"/>
+                    </Form.Item>
+                    <Form.Item name="password" rules={[{required: true, message: '请输入您的密码'}]}>
+                      <Input prefix={<LockOutlined/>} type="password" placeholder="请输入密码"/>
+                    </Form.Item>
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit" style={{width: '100%'}}>登录</Button>
+                    </Form.Item>
+                    <Form.Item wrapperCol={{offset: 13}}>
+                      <Space split={<Divider type="vertical"/>} size={0}>
+                        <a>忘记密码？</a>
+                        <a>免费注册</a>
+                      </Space>
+                    </Form.Item>
+                  </Form>
+                </TabPane>
+                <TabPane tab="邮箱登录" key="email">
+                  <Form name="email">
+                    <Form.Item name="email" rules={[{required: true, message: '请输入您的电子邮箱'}]}>
+                      <Input prefix={<MailOutlined/>} placeholder="请输入电子邮箱"/>
+                    </Form.Item>
+                    <Form.Item>
+                      <Row gutter={8}>
+                        <Col flex='auto'>
+                          <Input prefix={<LockOutlined/>} type="password" placeholder="请输入验证码"/>
+                        </Col>
+                        <Col flex='100px'>
+                          <Button>获取验证码</Button>
+                        </Col>
+                      </Row>
+                    </Form.Item>
+
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit" style={{width: '100%'}}>登录</Button>
+                    </Form.Item>
+                  </Form>
+                </TabPane>
+              </Tabs>
+            </Col>
+          </Row>
         </div>
         <div className={style.footer}>
-          border
+          Copyright © 2022-2023 upbos.com All Rights Reserved.
         </div>
       </div>
     </div>
